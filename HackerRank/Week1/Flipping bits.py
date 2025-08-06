0cce01f80129dfac0444e2aa879adca793474f3b
Jothish28 <jothishguntupalli@gmail.com>
2025-08-07 01:30:48 +0530

    day 3 hackerrank

---
+++ HackerRank/Week1/Flipping bits.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    bits = bin(n)[2:].zfill(32)
+    flipped = ''
+    for i in bits:
+        if i == '0':
+            flipped=flipped+'1'
+        else:
+            flipped = flipped+'0'
+    return int(flipped,2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ HackerRank/Week1/Lonely Integer.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    countdict={}
+    
+    for i in a:
+        if i in countdict:
+            countdict[i] = countdict[i] + 1
+        else:
+            countdict[i] = 1
+            
+    for number in countdict:
+        if countdict[number] == 1:
+            return number 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
